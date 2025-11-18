f80a2588bd5405fd866e1f391a0994a905891855
ujisoo <rjisoo.yoo@gmail.com>
2025-11-17 18:20:27 -0600

    adding lightrag shell scripts

---
+++ 395-scripts/lightrag/gpt-5-nano/run_novel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+python 395-scripts/lightrag/lightrag_test.py \
+    --subset novel \
+    --model_name gpt-5-nano \
+    --mode API \
+    --llm_base_url https://api.openai.com/v1 \
+    --embed_model BAAI/bge-large-en-v1.5 \
+    --base_dir 395-scripts/lightrag/gpt-4o-mini
